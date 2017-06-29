3ed88be6d9887fabe903b6644bf2440bfeec9754
Cqk <cqk0100@gmail.com>
2017-06-29 13:37:16 +0800

    add io functions

--- mcml/mcml_io.cpp
+++ mcml/mcml_io.cpp
@@ -1,3 +1,4 @@
+#include"utility_fwd.h"
 using std::vector;
 
 /*
@@ -19,3 +20,53 @@ void CriticalAngle(size_t Layer_num,vector<LayerClass>& layerspecs)
     }
 }
 
+
+
+
+/*
+Get 1D array elements by summing the 2D array elements.
+*/
+void Sum2DRd(const InputClass& In,OutClass& Out)
+{
+    size_t nr=In.input->nr;
+    size_t na=In.input->na;
+    size_t ir,ia;
+    AllocMatrix(Out.out->Rd_ra,nr,na);
+    AllocVector(Out.out->Rd_a,na);
+
+    double sum;
+
+	for (ir = 0; ir < nr; ir++) {
+		sum = 0.0;
+		for (ia = 0; ia < na; ia++) sum += Out.out->Rd_ra[ir][ia];
+		Out.out->Rd_r[ir] = sum;
+	}
+
+	for (ia = 0; ia < na; ia++) {
+		sum = 0.0;
+		for (ir = 0; ir < nr; ir++) sum += Out.out->Rd_ra[ir][ia];
+		Out.out->Rd_a[ia] = sum;
+
+	}
+
+	sum = 0.0;
+	for (ir = 0; ir < nr; ir++) sum += Out.out->Rd_r[ir];
+	Out.out->Rd = sum;
+}
+
+/*
+ * Return the index to the layer according to the index
+ * to the grid line system in z direction (Iz).
+ * Use the center of box.
+ */
+size_t IzToLayer(size_t Iz, InputStruct In_Parm)
+{
+	short i = 1; /* index to layer. */
+	short num_layers = In_Parm.num_layers;
+	double dz = In_Parm.dz;
+
+	while ((Iz + 0.5)*dz >= In_Parm.layerspecs[i].z1
+		&& i < num_layers) i++;
+
+	return(i);
+}
--- mcml/utility_fwd.h
+++ mcml/utility_fwd.h
@@ -11,6 +11,24 @@ double SpinTheta(double g);
 double RFresnel(double n1, double n2, double ca1, double& ca2_Ptr);
 
 void CriticalAngle(size_t Layer_num,vector<LayerClass>& layerspecs);
+void Sum2DRd(const InputClass& In,OutClass& Out);
+
+
+template<class Type>
+inline void AllocVector(vector<Type>& vec,size_t row)
+{
+    vec.resize(row+1);
+}
+
+
+template<class Type>
+inline void AllocMatrix(vector<vector<Type>>& mat,size_t row,size_t col)
+{
+    mat.resize(row+1);
+    for(size_t i=0;i<=col;++i)mat[i].resize(col+1);
+}
+
+
 
 
 #endif
